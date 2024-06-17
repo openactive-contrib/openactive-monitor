b77b1722f4c4a417b25ecd7d069a1be85d561351
Darren Temple <darren.temple@theodi.org>
2024-06-17 19:42:24 +0100

    - Modified job-get-feeds to use an environment variable for the output file path, in line with other codes - Modified job-get-feeds to use a file_out object to ensure proper file open/close

--- job-get-feeds/main.py
+++ job-get-feeds/main.py
@@ -2,10 +2,12 @@ import pickle
 import sys
 from datetime import datetime
 from openactive import get_feeds
+from os import getenv
 
 # --------------------------------------------------------------------------------------------------
 
 FILENAME_FEEDS = 'feeds.pickle'
+
 # This folder must have been made via the Google Cloud browser console under Cloud Storage for this
 # project, and the volume must have been mounted via the terminal at the mount-path '/volume-1'. With
 # this job called 'get-feeds', this was done as follows (note that the volume and its mount-path
@@ -13,7 +15,9 @@ FILENAME_FEEDS = 'feeds.pickle'
 #   $ gcloud beta run jobs update get-feeds \
 #   --add-volume name=volume-1,type=cloud-storage,bucket=openactive-all-data-harvester_cloudbuild \
 #   --add-volume-mount volume=volume-1,mount-path=/volume-1
-FILEPATH_RELATIVE_FEEDS = '../volume-1/data-feeds'
+FILEPATH_RELATIVE_FEEDS = getenv('FILEPATH_RELATIVE_FEEDS', '../volume-1/data-feeds')
+
+print('FILEPATH_RELATIVE_FEEDS:', FILEPATH_RELATIVE_FEEDS)
 
 # --------------------------------------------------------------------------------------------------
 
@@ -22,16 +26,17 @@ def run_get_feeds():
     feeds = get_feeds(flat=True)
     t2 = datetime.now()
 
-    pickle.dump(
-        {
-            'time_start': str(t1),
-            'time_finish': str(t2),
-            'time_taken': str(t2 - t1),
-            'num_feeds': len(feeds),
-            'feeds': feeds,
-        },
-        open(FILEPATH_RELATIVE_FEEDS + '/' + FILENAME_FEEDS, 'wb')
-    )
+    with open(FILEPATH_RELATIVE_FEEDS + '/' + FILENAME_FEEDS, 'wb') as file_out:
+        pickle.dump(
+            {
+                'time_start': str(t1),
+                'time_finish': str(t2),
+                'time_taken': str(t2 - t1),
+                'num_feeds': len(feeds),
+                'feeds': feeds,
+            },
+            file_out
+        )
 
 # --------------------------------------------------------------------------------------------------
 
