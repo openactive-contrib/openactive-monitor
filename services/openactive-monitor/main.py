64c605f9fddfbe6a68186784b51c56bbe548aa0a
Darren Temple <darren.temple@theodi.org>
2024-07-02 17:42:07 +0100

    - Modified openactive-monitor to align with jobs - Added /analysis endpoint

--- services/openactive-monitor/main.py
+++ services/openactive-monitor/main.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 from flask import Flask
+from os import getenv
 
 # --------------------------------------------------------------------------------------------------
 
@@ -8,12 +9,26 @@ app = Flask(__name__)
 
 # --------------------------------------------------------------------------------------------------
 
+# These folders must have been made via the Google Cloud browser console under Cloud Storage for this
+# project, and the volume must have been mounted via the terminal at the mount-path '/volume-1'. With
+# this service called 'openactive-monitor', this was done as follows (note that the volume and its mount-path
+# were given the same name, which didn't have to be so):
+#   $ gcloud beta run services update openactive-monitor \
+#   --region europe-west2 \
+#   --add-volume name=volume-1,type=cloud-storage,bucket=openactive-monitor_cloudbuild \
+#   --add-volume-mount volume=volume-1,mount-path=/volume-1
+RELATIVE_FILEPATH_FEEDS = getenv('RELATIVE_FILEPATH_FEEDS', '../volume-1/data-feeds')
+RELATIVE_FILEPATH_ANALYSIS = getenv('RELATIVE_FILEPATH_ANALYSIS', '../volume-1/data-analysis')
+
+FILENAME_FEEDS = 'feeds.pickle'
+FILENAME_ANALYSIS = 'analysis.pickle'
+
+# --------------------------------------------------------------------------------------------------
+
 @app.route('/')
 def main():
     try:
-        with open(f'../volume-1/data-analysis/analysis.pickle', 'rb') as file_in:
-            analysis = pickle.load(file_in)
-        return analysis
+        return 'OpenActive Monitor'
     except:
         return None
 
@@ -22,9 +37,20 @@ def main():
 @app.route('/feeds')
 def feeds():
     try:
-        with open(f'../volume-1/data-feeds/feeds.pickle', 'rb') as file_in:
+        with open(RELATIVE_FILEPATH_FEEDS + '/' + FILENAME_FEEDS, 'rb') as file_in:
             feeds = pickle.load(file_in)
         return feeds
+    except:
+        return None
+
+# --------------------------------------------------------------------------------------------------
+
+@app.route('/analysis')
+def analysis():
+    try:
+        with open(RELATIVE_FILEPATH_ANALYSIS + '/' + FILENAME_ANALYSIS, 'rb') as file_in:
+            analysis = pickle.load(file_in)
+        return analysis
     except:
         return None
 
