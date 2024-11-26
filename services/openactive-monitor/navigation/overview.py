7010da94442187df8e8ec2c44e1a57f6aa2342ee
Howard Askew <19271569+howaskew@users.noreply.github.com>
2024-11-26 16:21:24 +0000

    Swap bubble chart for logos

--- services/openactive-monitor/navigation/overview.py
+++ services/openactive-monitor/navigation/overview.py
@@ -5,164 +5,10 @@ from millify import millify
 import plotly.graph_objects as go
 import numpy as np
 import matplotlib.cm as cm
-
-class BubbleChart:
-    def __init__(self, area, bubble_spacing=0):
-        """
-        Setup for bubble collapse.
-
-        Parameters
-        ----------
-        area : array-like
-            Area of the bubbles.
-        bubble_spacing : float, default: 0
-            Minimal spacing between bubbles after collapsing.
-
-        Notes
-        -----
-        If "area" is sorted, the results might look weird.
-        """
-        area = np.asarray(area)
-        r = np.sqrt(area / np.pi)
-
-        self.bubble_spacing = bubble_spacing
-        self.bubbles = np.ones((len(area), 4))
-        self.bubbles[:, 2] = r
-        self.bubbles[:, 3] = area
-        self.maxstep = 2 * self.bubbles[:, 2].max() + self.bubble_spacing
-        self.step_dist = self.maxstep / 2
-
-        # calculate initial grid layout for bubbles
-        length = np.ceil(np.sqrt(len(self.bubbles)))
-        grid = np.arange(length) * self.maxstep
-        gx, gy = np.meshgrid(grid, grid)
-        self.bubbles[:, 0] = gx.flatten()[:len(self.bubbles)]
-        self.bubbles[:, 1] = gy.flatten()[:len(self.bubbles)]
-
-        self.com = self.center_of_mass()
-
-    def center_of_mass(self):
-        return np.average(
-            self.bubbles[:, :2], axis=0, weights=self.bubbles[:, 3]
-        )
-
-    def center_distance(self, bubble, bubbles):
-        return np.hypot(bubble[0] - bubbles[:, 0],
-                        bubble[1] - bubbles[:, 1])
-
-    def outline_distance(self, bubble, bubbles):
-        center_distance = self.center_distance(bubble, bubbles)
-        return center_distance - bubble[2] - \
-            bubbles[:, 2] - self.bubble_spacing
-
-    def check_collisions(self, bubble, bubbles):
-        distance = self.outline_distance(bubble, bubbles)
-        return len(distance[distance < 0])
-
-    def collides_with(self, bubble, bubbles):
-        distance = self.outline_distance(bubble, bubbles)
-        return np.argmin(distance, keepdims=True)
-
-    def collapse(self, n_iterations=50):
-        """
-        Move bubbles to the center of mass.
-
-        Parameters
-        ----------
-        n_iterations : int, default: 50
-            Number of moves to perform.
-        """
-        for _i in range(n_iterations):
-            moves = 0
-            for i in range(len(self.bubbles)):
-                rest_bub = np.delete(self.bubbles, i, 0)
-                # try to move directly towards the center of mass
-                # direction vector from bubble to the center of mass
-                dir_vec = self.com - self.bubbles[i, :2]
-
-                # shorten direction vector to have length of 1
-                dir_vec = dir_vec / np.sqrt(dir_vec.dot(dir_vec))
-
-                # calculate new bubble position
-                new_point = self.bubbles[i, :2] + dir_vec * self.step_dist
-                new_bubble = np.append(new_point, self.bubbles[i, 2:4])
-
-                # check whether new bubble collides with other bubbles
-                if not self.check_collisions(new_bubble, rest_bub):
-                    self.bubbles[i, :] = new_bubble
-                    self.com = self.center_of_mass()
-                    moves += 1
-                else:
-                    # try to move around a bubble that you collide with
-                    # find colliding bubble
-                    for colliding in self.collides_with(new_bubble, rest_bub):
-                        # calculate direction vector
-                        dir_vec = rest_bub[colliding, :2] - self.bubbles[i, :2]
-                        dir_vec = dir_vec / np.sqrt(dir_vec.dot(dir_vec))
-                        # calculate orthogonal vector
-                        orth = np.array([dir_vec[1], -dir_vec[0]])
-                        # test which direction to go
-                        new_point1 = (self.bubbles[i, :2] + orth *
-                                      self.step_dist)
-                        new_point2 = (self.bubbles[i, :2] - orth *
-                                      self.step_dist)
-                        dist1 = self.center_distance(
-                            self.com, np.array([new_point1]))
-                        dist2 = self.center_distance(
-                            self.com, np.array([new_point2]))
-                        new_point = new_point1 if dist1 < dist2 else new_point2
-                        new_bubble = np.append(new_point, self.bubbles[i, 2:4])
-                        if not self.check_collisions(new_bubble, rest_bub):
-                            self.bubbles[i, :] = new_bubble
-                            self.com = self.center_of_mass()
-
-            if moves / len(self.bubbles) < 0.1:
-                self.step_dist = self.step_dist / 2
-
-    def plot(self, ax, labels, colors):
-        """
-        Draw the bubble plot.
-
-        Parameters
-        ----------
-        ax : matplotlib.axes.Axes
-        labels : list
-            Labels of the bubbles.
-        colors : list
-            Colors of the bubbles.
-        """
-        for i in range(len(self.bubbles)):
-            circ = plt.Circle(
-                self.bubbles[i, :2], self.bubbles[i, 2]*0.95, color=colors[i]
-            )
-            ax.add_patch(circ)
-
-            # Split label if longer than 30 characters, word-wise
-            split_label = ""
-            current_line = ""
-            words = labels[i].split()
-            for word in words:
-                if len(current_line + word) <= 12:
-                    current_line += word + " "
-                else:
-                    split_label += current_line.strip() + "\n"
-                    current_line = word + " "
-            split_label += current_line.strip()  # Add the last line
-
-
-            ax.text(
-                *self.bubbles[i, :2], 
-                f"{split_label}\n({int(self.bubbles[i, 3])})",  # Add count to label
-                horizontalalignment='center',
-                verticalalignment='center',
-                color='white',
-                fontsize=8,
-                wrap=True
-            )
-
+import time 
 # --------------------------------------------------------------------------------------------------
 
-tabs = st.tabs(['Ecosystem Overview', 'KPIs'])
+tabs = st.tabs(['Snapshot', 'KPIs'])
 
 with tabs[0]:
 
@@ -188,24 +34,13 @@ with tabs[0]:
             if button1:
                 content.empty()
                 st.markdown("**OpenActive** is a decentralised open data initiative. Each data provider shares one or more data feeds, providing near real time availability of their activities and facilities.")
-                cols = st.columns([1, 4, 1])
-                with cols[1]:
-                    # Create a colormap instance
-                    cmap = cm.get_cmap('viridis')
-                    # Map the colormap to your data (classification categories)
-                    norm = plt.Normalize(vmin=0, vmax=len(st.session_state.publishers['Classification']))
-                    colors = [cmap(norm(i)) for i in range(len(st.session_state.publishers['Classification']))]
-                    bubble_chart = BubbleChart(area=st.session_state.publishers['Count'], bubble_spacing=0.01)
-                    bubble_chart.collapse()
-                    fig, ax = plt.subplots(subplot_kw=dict(aspect="equal"))  # Adjust figsize as needed
-                    bubble_chart.plot(ax, st.session_state.publishers['Classification'], colors)
-                    ax.axis("off")
-                    ax.relim()
-                    ax.autoscale_view()
-                    # Display the chart in Streamlit
-                    st.pyplot(fig, use_container_width=True)
-                    plt.close(fig)  # Close the figure to avoid potential issues
-                
+                # Use st.empty to create a placeholder for the image
+                image_placeholder = st.empty()
+                while button1:  # Loop until button is clicked again
+                    for url in logo_urls:
+                        with image_placeholder.container():  # Use a container for clean updates
+                            st.image(url, width=100)
+                            time.sleep(0.3)
                 st.markdown("The **status page** lists the data providers and basic information about the status of each feed.")
             elif button2:
                 content.empty()
