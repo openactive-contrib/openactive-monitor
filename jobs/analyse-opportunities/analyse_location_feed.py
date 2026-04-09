cf978077636da5b8d19347f4628f2b3f541b6976
Huseyin Kir <73784267+hkir-dev@users.noreply.github.com>
2026-04-09 13:48:13 +0100

    cli example added

--- jobs/analyse-opportunities/analyse_location_feed.py
+++ jobs/analyse-opportunities/analyse_location_feed.py
@@ -11,6 +11,11 @@ python analyse_location_feed.py -g ../volume-1/data-analysis/000-location-nhstru
 
 python analyse_location_feed.py -g ../volume-1/data-analysis/000-location-gps.geojson -n Name -d 16-02-2026 -o ./output9 -v
 
+python analyse_location_feed.py \            
+            -g ../volume-1/data-analysis/000-location-parishes.geojson \
+            -n PARNCP25NM \
+            -o ./output7 \
+            -v
 
 """
 
